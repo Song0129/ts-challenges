d1932cb522b1cef923fd9ebf5f50b8dc48d00362
Song0129 <Song_3191@163.com>
2022-03-15 16:20:08 +0800

    📝 更新文件

--- 4-easy-pick/template.ts
+++ 4-easy-pick/template.ts
@@ -1,5 +1,10 @@
 // ts 联合类型 union  "title" | "completed"
 
+// title
+// 循环
+// [title,completed]
+// [title,completed,description]
+// 上下一一对比
 type MyPick<T, K extends keyof T> = {
 	[P in K]: T[P];
 };
@@ -19,5 +24,11 @@ function muPick(todo, keys) {
 
 // 1.返回一个对象
 // 2.遍历foreach   ts遍历的是union    "title" | "completed"  mapped
-// 3.todo[key]取值
+// -https://www.typescriptlang.org/docs/handbook/2/mapped-types.html
+// 3.todo[key]取值  indexed
+// -https://www.typescriptlang.org/docs/handbook/2/indexed-access-types.html
 // 4.看看key在不在todo里面
+//    1.keyof lookup
+//      -https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-1.html#keyof-and-lookup-types
+//    2.extends 在<>内表示约束
+//      -https://www.typescriptlang.org/docs/handbook/2/generics.html#generic-constraints
