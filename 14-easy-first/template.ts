23c17558b1d02bada8ac01ab02d5a0d8ebd7fbdd
Song0129 <Song_3191@163.com>
2022-03-22 16:10:51 +0800

    📝 提交easy-first案例

--- 14-easy-first/template.ts
+++ 14-easy-first/template.ts
@@ -1,15 +1,28 @@
 // type First<T extends any[]> = T extends [] ? never : T[0];
-type First<T extends any[]> = T["length"] extends 0 ? never : T[0];
+// type First<T extends any[]> = T["length"] extends 0 ? never : T[0];
+// type First<T extends any[]> = T[0] extends T[number] ? T[0] : never;
+type First<T extends any[]> = T extends [infer First, ...infer Rest] ? First : never;
 
-let tuple = ["3", "2", "1"];
+// T[number]
+type ages = [];
 
-type r = First<typeof tuple>;
+// union
+type t1 = ages[number];
+
+// 看看 某个值在不在 union 里
+// 1 extends 1
+// 1 extends 2
+// 1 extends 3
+type t2 = 1 extends ages[number] ? "true" : "false";
 
 // js
 const first = arr => {
 	// arr 是不是一个空数组 如果是的话 那么返回 never
-	if (arr.length === 0) return "never";
-	return arr[0];
+	const [first, ...rest] = arr;
+	return first ? first : "never";
+
+	// if (arr.length === 0) return "never";
+	// return arr[0];
 };
 
 // 知识点：
