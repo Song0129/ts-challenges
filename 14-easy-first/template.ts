60d7040a1744e5c84abdddaa9816a60ce73aad06
Song0129 <Song_3191@163.com>
2022-03-22 15:57:02 +0800

    📝 提交easy-first案例

--- 14-easy-first/template.ts
+++ 14-easy-first/template.ts
@@ -1,5 +1,4 @@
-// type First<T extends any[]> = T extends [] ? never : T[0];
-type First<T extends any[]> = T[0];
+type First<T extends any[]> = T extends [] ? never : T[0];
 
 let tuple = ["3", "2", "1"];
 
@@ -8,5 +7,12 @@ type r = First<typeof tuple>;
 // js
 const first = arr => {
 	// arr 是不是一个空数组 如果是的话 那么返回 never
+	if (arr.length === 0) return "never";
 	return arr[0];
 };
+
+// 知识点：
+// 1. extends 类型判断
+// 2. 获取 tuple 的 length 属性
+// 3. extends union 判断的规则
+// 4. inter 的使用
