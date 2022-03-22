81989450425cd59f3a2a20287354a64edb728319
Song0129 <Song_3191@163.com>
2022-03-22 15:54:11 +0800

    📝 提交easy-first案例

--- 14-easy-first/template.ts
+++ 14-easy-first/template.ts
@@ -1,5 +1,12 @@
-type First<T extends any[]> = T extends [] ? never : T[0];
+// type First<T extends any[]> = T extends [] ? never : T[0];
+type First<T extends any[]> = T[0];
 
 let tuple = ["3", "2", "1"];
 
 type r = First<typeof tuple>;
+
+// js
+const first = arr => {
+	// arr 是不是一个空数组 如果是的话 那么返回 never
+	return arr[0];
+};
