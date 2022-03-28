fe9e4a656800c1ca5c5579d2c07de0a060a139aa
Song0129 <Song_3191@163.com>
2022-03-28 15:49:03 +0800

    📝 提交easy-exclude案例

---
+++ 43-easy-exclude/readme.md
@@ -0,0 +1,8 @@
+<h1>
+Exclude 
+<img src="https://img.shields.io/badge/-%E7%AE%80%E5%8D%95-7aad0c" alt="简单"/> 
+<img src="https://img.shields.io/badge/-%23built--in-999" alt="#built-in"/></h1>
+
+实现内置的 Exclude <T，U>类型，但不能直接使用它本身。
+
+> 从联合类型 T 中排除 U 的类型成员，来构造一个新的类型。
---
+++ 43-easy-exclude/template.ts
@@ -0,0 +1,27 @@
+type MyExclude<T, U> = T extends U ? never : T;
+
+// jS
+
+// ["a","b","c"]
+// ["a"]
+
+function MyExclude(T, U) {
+	const result = [];
+	for (let i = 0; i < T.length; i++) {
+		const t = T[i];
+		let boo = false;
+		// for (let j = 0; j < U.length; j++) {
+		// 	const u = U[j];
+		// 	if (t === u) {
+		// 		boo = true;
+		// 	}
+		// }
+		// if (!boo) {
+		// 	result.push(t);
+		// }
+		if (!U.includes(t)) {
+			result.push(t);
+		}
+	}
+	return result;
+}
